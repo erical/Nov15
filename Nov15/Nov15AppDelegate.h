37297409206f3b866247bb1f005e1c275863ead8
Erica Lederman <erical55@hotmail.com>
2012-11-14 20:37:31 -0500

    Initial Commit

---
+++ Nov15/Nov15AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Nov15AppDelegate.h
+//  Nov15
+//
+//  Created by Erica Lederman on 11/14/12.
+//  Copyright (c) 2012 Erica Lederman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Nov15AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
